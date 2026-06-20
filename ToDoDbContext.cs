b55b748ecfdbddc09100f476441e5619ea168d91
miriam315 <miriam67839@gmail.com>
2026-06-20 23:48:52 +0300

    Items Users

--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -31,7 +31,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("items");
+            entity.ToTable("Items");
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
@@ -40,7 +40,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("users");
+            entity.ToTable("Users");
 
             entity.Property(e => e.Name).HasMaxLength(100);
             entity.Property(e => e.Password).HasMaxLength(10);
